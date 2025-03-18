1099d2a1453a65b82b66717081a3506b484df8d1
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-03-18 22:23:37 +0500

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthController } from "./health/health.controller";
 
 @Module({
   imports: [DatabaseModule, UsersModule, AuthModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
