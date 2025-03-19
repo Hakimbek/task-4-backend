58faacb8a0d25d0351e2357e35f689327de74cbf
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-03-19 11:58:05 +0500

    commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,7 +5,10 @@ import {
   Delete,
   Body,
   UseGuards,
+  Res,
+  HttpStatus
 } from '@nestjs/common';
+import { Response } from 'express';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
@@ -22,13 +25,23 @@ export class UserController {
 
   @Patch('change-status')
   @UseGuards(JwtAuthGuard)
-  async changeStatus(@Body('users') users: { id: string; isActive: boolean }[]) {
-    return this.userService.changeStatus(users);
+  async changeStatus(@Body('users') users: { id: string; isActive: boolean }[], @Res() res: Response) {
+    try {
+      const updatedUsers = this.userService.changeStatus(users);
+      res.status(HttpStatus.OK).send(`${updatedUsers} users are updated successfully.`);
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).send(e.message);
+    }
   }
 
   @Delete()
   @UseGuards(JwtAuthGuard)
-  async deleteUser(@Body('ids') ids: string[]) {
-    return this.userService.deleteUser(ids);
+  async deleteUser(@Body('ids') ids: string[], @Res() res: Response) {
+    try {
+      const deletedUsers = this.userService.deleteUser(ids);
+      res.status(HttpStatus.OK).send(`${deletedUsers} users are deleted successfully.`);
+    } catch (e) {
+      res.status(HttpStatus.BAD_REQUEST).send(e.message);
+    }
   }
 }
