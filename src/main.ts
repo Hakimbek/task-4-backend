1270f67a58789c46e9666e0b079b45ab864bce1a
Khakim Bakhramov <khakimbakhramov@gmail.com>
2025-03-19 20:46:09 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config();
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
